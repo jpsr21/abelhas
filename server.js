b0c6cdee646ba3f0af5655f2ac0dd64303f7755f
jpsr21 <31295401+jpsr21@users.noreply.github.com>
2025-03-11 11:14:29 -0300

    Ajuste de porta para Render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para permitir requisições JSON e CORS
 app.use(cors());
