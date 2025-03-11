4b243dfc0aa453e9acbc87fca7aa0971220268df
jpsr21 <31295401+jpsr21@users.noreply.github.com>
2025-03-11 13:26:20 -0300

    teste

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ let ultimoDado = {}; // Armazena o último dado gerado
 // Função para gerar valores aleatórios (simulação de sensores)
 function gerarDados() {
   return {
-    temperatura: (Math.random() * (35 - 20) + 20).toFixed(1), // 20°C a 35°C
-    umidade: (Math.random() * (90 - 40) + 40).toFixed(1), // 40% a 90%
-    peso: (Math.random() * (10 - 5) + 5).toFixed(2), // 5kg a 10kg
-    co2: Math.floor(Math.random() * (800 - 300) + 300) // 300ppm a 800ppm
+    temperatura: 30
+    umidade: 70
+    peso: 10
+    co2: 500
   };
 }
 
